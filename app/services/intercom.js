c882faf70d45f6b27623efe6ce63676e8aaa098e
Davide Papagni <davide@algonauti.com>
2016-05-16 18:05:22 +0200

    automatically pass app_id param to Intercom

--- app/services/intercom.js
+++ app/services/intercom.js
@@ -1,1 +1,13 @@
-export { default } from 'ember-cli-intercom/services/intercom';
+import Ember from 'ember';
+import config from '../config/environment';
+
+export default Ember.Service.extend({
+  boot(params) {
+    params['app_id'] = config['ember-cli-intercom'].appId;
+    Intercom('boot', params);
+  },
+
+  update(params) {
+    Intercom('update', params);
+  }
+});
