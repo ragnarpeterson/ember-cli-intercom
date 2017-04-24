260a39df94fa03d0e8009207f9db0268592494a5
Tomas Spacek <tomspacek@gmail.com>
2017-04-25 01:02:40 +0200

    Disable Intercom if appId is null
    (cherry picked from commit f6cbc03)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
   name: require('./package').name,
 
   contentFor: function(type, config) {
-    if (type === 'head') {
+    if (type === 'head' && config['ember-cli-intercom'].appId) {
       return "<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/" + config['ember-cli-intercom'].appId + "';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>";
     }
   }
